13db3904f77debf575dc822b76761cd7fcbac37b
Kim Brown <kim.brown.2442@gmail.com>
2023-05-02 00:02:52 -0500

    Split into main and poem_generator files

--- main.py
+++ main.py
@@ -1,152 +1,66 @@
 # https://realpython.com/python-gui-tkinter/
-import random
 import tkinter as tk
-import requests
+from poem_generator import *
 
 num_poets = 1
-num_poems = 2
+num_poems = 3
 max_lines = 5
 main_window_width = 800
 main_window_height = 800
-
-titles_url = "https://poetrydb.org/author/{}/title"
-poem_text_url = "https://poetrydb.org/title/{}/lines.json"
-poets_url = "https://poetrydb.org/author"
-saved = False  # prevents saving poem more than once
-
-
-def get_poets() -> list:
-    resp = requests.get(poets_url)
-    if resp.status_code != 200:
-        print("Failed with status {}, please try again.".format(resp.status_code))
-        return []
-
-    resp_json = resp.json()  # TODO: catch errors
-    if "authors" not in resp_json:
-        return []
-
-    return resp_json["authors"]
-
+button_width = 25
+button_height = 5
 
 poet_options = get_poets()
 
 
-def get_poem() -> str:
-    global saved
-    all_lines = []
-    for poet in [poet_choice.get()]:  # TODO: adjust when supporting more than one poet
-        titles = get_titles(poet)
-        # print("Found {} titles for this author. Randomly picking {}\n".format(len(titles), num_poems))
-        rand_poems = pick_from_list(titles, num_poems)
-        for title in rand_poems:
-            lines = get_poem_lines(title)
-            all_lines = all_lines + lines
-    result = pick_from_list(all_lines, int(num_lines.get()[0]))
-    clean_output_lines(result)
-    saved = False
-    return '\n\n'.join(result)
-
-
-def get_titles(poet) -> list:
-    resp = requests.get(titles_url.format(poet))
-    if resp.status_code != 200:
-        return []
-
-    titles = []
-    for title in resp.json():  # TODO: catch errors here
-        titles.append(title["title"])
-
-    return titles
-
-
-# pick_from_list: returns cnt unique random items in the list
-def pick_from_list(options: list, cnt: int) -> list:
-    options_copy = options.copy()  # don't shuffle input list
-    random.shuffle(options_copy)
-    return options_copy[:cnt]
-
-
-def filter_poem_lines(lines: list) -> list:
-    output = []
-    for line in lines:
-        stripped_line = line.strip()
-        if len(stripped_line) > 1:
-            output.append(stripped_line)
-
-    return output
-
-
-def get_poem_lines(name: str) -> list:
-    resp = requests.get(poem_text_url.format(name))
-    if resp.status_code != 200:
-        print("Failed to get poems- please try again")
-        return []
-
-    return filter_poem_lines(resp.json()[0]["lines"])  # TODO: catch errors here
-
-
-def clean_output_lines(lines: list):
-    for i in range(len(lines)):
-        lines[i] = lines[i].replace("\xad", "- ").replace("\"", "")
-    last_line = lines[len(lines)-1]
-    last_char = last_line[len(last_line)-1]
-    if last_char == "," or last_char == ";":
-        lines[len(lines)-1] = last_line[:-1]
-
-
 def show_poem():
-    poem_text.set(get_poem())
+    poem_text.set(get_poem(num_poems, int(num_lines.get()[0]), poet_choice.get()))
     label.config(text=poem_text.get())
 
 
-def save():
-    global saved
-    if saved:
-        return
-    with open('created.txt', 'a') as f:
-        f.write("{}\n\n--{}\n\n\n\n\n\n".format(poem_text.get(), poet_choice.get()))
-        f.close()
-    saved = True
+def save_poem():
+    save(poem_text.get(), poet_choice.get())
 
 
 window = tk.Tk()
 window.title("Poem Remixer")
 window.geometry("{}x{}".format(main_window_width, main_window_height))
 poet_choice = tk.StringVar()
-poet_choice.set("Ambrose Bierce")
+poet_choice.set(poet_options[0])
 poem_text = tk.StringVar()
 
-num_lines = tk.StringVar()
-num_lines.set("3 lines")
 line_number_options = ["1 line"]
 for i in range(2, max_lines+1):
     line_number_options.append("{} lines".format(i))
+num_lines = tk.StringVar()
+num_lines.set(line_number_options[2])
 
 num_lines_dropdown = tk.OptionMenu(window, num_lines, *line_number_options)
 num_lines_dropdown.grid(row=0, column=1)
 
-poem_frame = tk.Frame(window, width=main_window_width/2, height=main_window_height/2, highlightbackground="blue", highlightthickness=2)
+poem_frame = tk.Frame(window, width=main_window_width/2, height=main_window_height/2,
+                      highlightbackground="blue", highlightthickness=2)
 poem_frame.grid(row=1, column=0)
 
 label = tk.Label(window, text=poem_text.get())
 label.grid(row=1, column=0)
 
 poet_dropdown = tk.OptionMenu(window, poet_choice, *poet_options)
-poet_dropdown.grid(row=0, column=0, pady=30)
+poet_dropdown.grid(row=0, column=0)
 generate_button = tk.Button(
     text="Generate",
     command=show_poem,
-    width=25,
-    height=5,
+    width=button_width,
+    height=button_height,
     bg="white",
     fg="black",
 )
 generate_button.grid(row=2, column=0)
 save_button = tk.Button(
     text="Save poem",
-    command=save,
-    width=25,
-    height=5,
+    command=save_poem,
+    width=button_width,
+    height=button_height,
     bg="white",
     fg="black",
 )
---
+++ poem_generator.py
@@ -0,0 +1,93 @@
+import requests
+from random import *
+
+poets_url = "https://poetrydb.org/author"
+titles_url = "https://poetrydb.org/author/{}/title"
+poem_text_url = "https://poetrydb.org/title/{}/lines.json"
+saved = False
+
+
+def get_poets() -> list:
+    resp = requests.get(poets_url)
+    if resp.status_code != 200:
+        print("Failed with status {}, please try again.".format(resp.status_code))
+        return []
+
+    resp_json = resp.json()  # TODO: catch errors
+    if "authors" not in resp_json:
+        return []
+
+    return resp_json["authors"]
+
+
+def get_titles(poet) -> list:
+    resp = requests.get(titles_url.format(poet))
+    if resp.status_code != 200:
+        return []
+
+    titles = []
+    for title in resp.json():  # TODO: catch errors here
+        titles.append(title["title"])
+
+    return titles
+
+
+def get_poem(num_poems, num_lines: int, poet_choice: str) -> str:
+    global saved
+    all_lines = []
+    for poet in [poet_choice]:  # TODO: adjust when supporting more than one poet
+        titles = get_titles(poet)
+        # print("Found {} titles for this author. Randomly picking {}\n".format(len(titles), num_poems))
+        rand_poems = pick_from_list(titles, num_poems)
+        for title in rand_poems:
+            lines = get_poem_lines(title)
+            all_lines = all_lines + lines
+    result = pick_from_list(all_lines, num_lines)
+    clean_output_lines(result)
+    saved = False
+    return '\n\n'.join(result)
+
+
+# pick_from_list: returns cnt unique random items in the list
+def pick_from_list(options: list, cnt: int) -> list:
+    options_copy = options.copy()  # don't shuffle input list
+    shuffle(options_copy)
+    return options_copy[:cnt]
+
+
+def clean_output_lines(lines: list):
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace("\xad", "- ").replace("\"", "")
+    last_line = lines[len(lines)-1]
+    last_char = last_line[len(last_line)-1]
+    if last_char == "," or last_char == ";":
+        lines[len(lines)-1] = last_line[:-1]
+
+
+def get_poem_lines(name: str) -> list:
+    resp = requests.get(poem_text_url.format(name))
+    if resp.status_code != 200:
+        print("Failed to get poems- please try again")
+        return []
+
+    return filter_poem_lines(resp.json()[0]["lines"])  # TODO: catch errors here
+
+
+def filter_poem_lines(lines: list) -> list:
+    output = []
+    for line in lines:
+        stripped_line = line.strip()
+        if len(stripped_line) > 1:
+            output.append(stripped_line)
+
+    return output
+
+
+def save(poem_text, poet: str):
+    global saved
+    if saved:
+        return
+    with open('created.txt', 'a') as f:
+        f.write("{}\n\n--{}\n\n\n\n\n\n".format(poem_text, poet))
+        f.close()
+    saved = True
